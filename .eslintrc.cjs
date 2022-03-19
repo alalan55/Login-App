405037ffefadefcf26422e13d9c3bea76e81bf49
aoliveira <aoliveira@keener.io>
2022-03-19 17:30:10 -0300

    Inicia a construção da página de autenticação

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,5 +10,8 @@ module.exports = {
   ],
   "env": {
     "vue/setup-compiler-macros": true
+  },
+  "rules":{
+    "prettier/prettier": 0
   }
 }
---
+++ src/components/bosons/AuthRegisterLayout.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="auth-registe-layout">
+        <div class="component">
+            alan de oliveira
+            <router-view/>
+        </div>
+        <div class="img"></div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+.auth-registe-layout{
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    &>div{
+        border: 1px solid;
+        width: 100%;
+        height: 100%;
+    }
+}
+
+</style>
