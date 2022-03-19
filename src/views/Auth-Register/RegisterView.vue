4e0f66c68237af5fc64a14571ed522d7db3ec792
aoliveira <aoliveira@keener.io>
2022-03-19 18:47:27 -0300

    Insere navegação entre as rotas de login

---
+++ src/views/Auth-Register/RegisterView.vue
@@ -0,0 +1,17 @@
+<template>
+    <RegisterTemplate/>
+</template>
+
+<script>
+import RegisterTemplate from '../../components/templates/RegisterTemplate.vue'
+export default {
+    components:{
+        RegisterTemplate
+    },
+    setup () {
+        
+
+        return {}
+    }
+}
+</script>
