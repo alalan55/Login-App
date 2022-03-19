4a0a826ebc496116514187ac48c2c61d1c8690be
aoliveira <aoliveira@keener.io>
2022-03-19 19:44:41 -0300

    Insere realização de registro de usuário, e inserção de mensagem de sucesso, caso o mesmo ocorra

--- src/stores/login.js
+++ src/stores/login.js
@@ -4,11 +4,20 @@ import { supabase } from '../services/supabase'
 export const useLoginStore = defineStore({
     id: 'login',
     state: () => ({
-        uer: {}
+        user: {},
+        userId: null
     }),
+    getters:{
+        $userId(state){
+            return state.userId
+        },
+        $user(state){
+            return state.user
+        }
+    },
     actions: {
         async login(user) {
-            console.log('usuairo no state', user)
+            console.log('usuairo no state, login', user)
             try {
                 let req = await supabase.auth.signIn(user)
                 console.log(req)
@@ -16,6 +25,16 @@ export const useLoginStore = defineStore({
                 console.error(error)
             }
 
+        },
+        async register (user){
+            try {
+                let req = await supabase.auth.signUp(user)
+                this.user = req.user
+                this.userId = req.user.id
+                return req.user
+            } catch (error) {
+                console.error(error)
+            }
         }
     }
 })
